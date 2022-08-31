e84ac5428518f23a3eb0c906c283c92d0bb7f4d9
fer <fernando452000@gmail.com>
2022-08-31 17:54:37 -0500

    Se indetó mejor, Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+//Click
 const menuEmail = document.querySelector('.navbar-email');
+const hamburguesa = document.querySelector('.menu');
+
+//Display
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', tooggleDesktopMenu);
+hamburguesa.addEventListener('click', tooggleMobileMenu);
 
 function tooggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function tooggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
